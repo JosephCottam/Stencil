8a163c67f0fafa61b50b82b2773a4f1d082eeef9
Joseph Cottam <jcottam@indiana.edu>
2012-04-04 11:01:06 -0400

    Tests for module metadata loading.

---
+++ Test/stencil/unittests/module/java/TestModuleMetadata.java
@@ -0,0 +1,9 @@
+package stencil.unittests.module.java;
+
+
+import stencil.modules.java.JavaC;
+import stencil.unittests.module.ModuleMetadataBase;
+
+public class TestModuleMetadata extends ModuleMetadataBase {
+	public TestModuleMetadata() {super(JavaC.class, 1);}
+}
