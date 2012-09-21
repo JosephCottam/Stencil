8c22af6e442a2228aebe45ca89e1c51cd6cb9798
Joseph Cottam <jcottam@indiana.edu>
2012-09-21 17:21:54 -0400

    WIP: Parser improving.

--- Cardenio/Stencil/src/stencil/core.clj
+++ Cardenio/Stencil/src/stencil/core.clj
@@ -1,21 +1,23 @@
-(ns stencil.core)
-   (:use stencil.rparse :only [parse]))
+(ns stencil.core
+   (:require stencil.rparse))
 
 (defn -main [from to]
   (println "Compling from" from "to" to "(but not really)"))
 
-;;Directions for reges -- http://stackoverflow.com/questions/5695240/php-regex-to-ignore-escaped-quotes-within-quotes
+;Directions for reges -- http://stackoverflow.com/questions/5695240/php-regex-to-ignore-escaped-quotes-within-quotes
+
+(defn parseProgram [program]
+  "string -> tree: Parses a stencil program from a string."
+  (stencil.rparse/parseProgram program)) 
 
 (defn readProgram 
-  "filename -> tree: reads tree from specified file
-   REQUIRES that all non-syntax colons be escaped.
-   This is required because we ': isn't a valid symbol in Clojure
-   and we use a regular expression to convert them to a valid symbol (namely, stBind)
-   before reading the file contents into an s-expression."
+  "filename -> tree: reads tree from specified file"
   [filename] 
-  (let [raw (str \( (slurp filename) \n \))]
-     (parse raw #"[\s,]+" #"(?:[^"\\]|\\.)*" "(" ")" nil)))
-        
+    (let [f (new java.io.File filename)
+          name (re-find #"[^.]*" (.getName f))]
+    (parseProgram (str "(stencil" name (slurp filename) \n \)))))
+      
 (defn normalize 
   "tree -> tree: Transforms a parse-form tree to normal-form tree"
   [program] (-> program identity))
+
--- Cardenio/Stencil/src/stencil/rparse.clj
+++ Cardenio/Stencil/src/stencil/rparse.clj
@@ -1,25 +1,68 @@
+;Based roughly on -- http://thingsaaronmade.com/blog/writing-an-s-expression-parser-in-ruby.html 
 (ns stencil.rparse
   "Not-quite simple parser.
    Takes in balanced-delimeter trees and eventually produces s-expressions from them.
    Has a simple 'reader-macro' system, so the inputs can be not-quite s-expressions.")
 
-(defn parse [source split exclude open close transforms]
-   "Convert a source string into an s-expression.
-    Tokens are split appart using split, but regions that match exclude will be returned as a single block
-    open/close -- tokens corresponding to "down" and "up" in the tree
-    transforms -- list of pred/trans pairs.  If the pred passes, trans is applied and its result placed in the tree."
-   (-> source 
-      (tokenize split exclude)
-      (makeTree delim transforms)))
+(defn insertAlters [sentinel tokens alters]
+  (cond 
+    (empty? alters) tokens
+    (empty? tokens) '()
+    (= sentinel (first tokens)) (cons (first alters) (insertAlters sentinel (rest tokens) (rest alters)))
+    :else (cons (first tokens) (insertAlters sentinel (rest tokens) alters))))
 
-
-(defn tokenize [source split exclude]
-   "Produce an sequence of tokens."
-    nil)
-
-
-(defn makeTree [tokens open close transforms]
+(defn makeTree [tokens open close emit]
    "Produce an s-expression using open/close to do nesting and transforms to patch over non-compliant regions."
-    nil)
+  (cond 
+    (empty? tokens) 
+       '(() ())
+
+    (not (nil? (re-matches open (first tokens))))
+       (let [[phase1 remain] (makeTree (rest tokens) open close emit)
+             [phase2 remain] (makeTree remain open close emit)]
+         (list (cons phase1 phase2) remain))
+
+    (not (nil? (re-matches close (first tokens)))) 
+       (list '() (rest tokens))
+    
+    :else (let [[phase1 remain] (emit tokens)
+                [phase2 remain] (makeTree remain open close emit)]
+            (list (cons phase1 phase2) remain))))
+         
+
+
+(defn emitter [transforms]
+  "Produce a function that take tokens are returns value/token-seq pairs from the transforms"
+  (fn [tokens]
+    (loop [transforms transforms]
+      (let [[p t] (first transforms)]
+        (cond
+          (and (fn? t) (p (first tokens))) (t tokens)
+          (nil? t) (list (read-string (first tokens)) (rest tokens))
+          :else (recur (rest transforms)))))))
           
+(defn parse [source split exclude placeHolder open close transforms]
+   "Convert a source string into an s-expression.
+    split -- divide up tokens, must include white-space (sorry!)
+    exclude -- finds large blocks where split should not be applied (like string-literals)
+    placeHolder -- used to protect excluded items, MUST NOT be divdied up by split
+    open/close -- patterns for moving up and down in the tree, must include any possible match in one group
+    transforms -- list of pred/trans pairs.  If the pred passes, trans is applied to the curent token stream.
+                  Trans returns a value and an advanced token stream."
+  (let [bigTokens (re-seq exclude source)
+        protected (clojure.string/replace source exclude placeHolder)
+        expanded  (.trim (clojure.string/replace (clojure.string/replace protected open " $1 ") close " $1 "))
+        pTokens   (clojure.string/split expanded split)
+        tokens    (insertAlters placeHolder pTokens bigTokens)
+        [tree _]  (makeTree tokens open close (emitter transforms))]
+     tree))
+
+
+(defn bind? [token] (.equals ":" token))
+(defn bind [tokens] (list "op:" (rest tokens))) 
+
+(defn parseProgram [program]
+  "string -> tree: Parses a stencil program from a string."
+  (parse program #"[\s,]+" #"\"([^\"\\]|\\.)*\"" "__++STRING_LITERAL++__" #"(\()" #"(\))" `((~bind? ~bind)))) 
+
 
