e4d60f4bd6bae70271a14e380eedd78cbd88d971
Joseph Cottam <jcottam@indiana.edu>
2012-09-21 01:01:44 -0400

    Started rparse version.

--- Cardenio/Stencil/src/Stencil/core.clj
+++ Cardenio/Stencil/src/Stencil/core.clj
@@ -1,5 +1,15 @@
-(ns Stencil.core)
+(ns Stencil.core
+   (:use stencil.rparse :only [parse]))
 
-(defn readProgram [filename] (read-string (str \( (slurp filename) \n \))))
+
+;;Directions for reges -- http://stackoverflow.com/questions/5695240/php-regex-to-ignore-escaped-quotes-within-quotes
+
+(defn readProgram [filename] 
+  (let [raw (str \( (slurp filename) \n \))]
+     (parse raw #"[\s,]+" #"(?:[^"\\]|\\.)*" "(" ")" nil)))
+        
+        
+
+(xxread-string (str \( (slurp filename) \n \))))
 
 (defn transform [program] (-> program identity))
