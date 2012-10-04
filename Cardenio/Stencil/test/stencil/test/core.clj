0b5d943524cb91f0d4cc58ef5402fac56a79d595
Joseph Cottam <jcottam@indiana.edu>
2012-10-04 16:04:18 -0400

    String template added (getting ready for code generation)

--- Cardenio/Stencil/test/stencil/test/core.clj
+++ Cardenio/Stencil/test/stencil/test/core.clj
@@ -1,7 +1,4 @@
 (ns Stencil.test.core
   (:use [stencil.core])
-  (:use [stencil.transform])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
