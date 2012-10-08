a04dd0aae402a3d915beb38b00972d3f99ee3080
Joseph Cottam <jcottam@indiana.edu>
2012-10-08 16:18:36 -0400

    More tests...

--- Cardenio/Stencil/test/stencil/test/core.clj
+++ Cardenio/Stencil/test/stencil/test/core.clj
@@ -2,9 +2,18 @@
   (:use [stencil.core])
   (:use [clojure.test]))
 
+(def ps parseStencil)
+
 (deftest parsing 
-  (is (= (parseStencil "(a)") '(a)))
-  (is (= (parseStencil "(stencil Test)") '(stencil Test)))
-  (is (= (parseStencil "#(a,b,c)") '($tuple a b c)))
-  (is (= (parseStencil "#(a b c)") '($tuple a b c)))
+  (is (= (ps "(a)") '(a)))
+  (is (= (ps "(stencil Test)") '(stencil Test)))
+  (is (= (ps "#(a,b,c)") '($tuple a b c)))
+  (is (= (ps "#(a b c)") '($tuple a b c)))
+  (is (= (ps "##(a b c)") '($ptuple a b c)))
+  (is (= (ps "##())") '($ptuple)))
+  (is (= (ps "(a : [int])") '(a (meta (int)))))
+  (is (= (ps "(a : b)") '(a $op-colon b)))
+  (is (= (ps "; some") '(comment " some")))
+  (is (= (ps ";* some then more*;")) '(comment " some then more"))
+  (is (= (ps "(;* some*; then more)")) '((comment " some") then more))
 )
---
+++ Cardenio/Stencil/test/stencil/test/transform.clj
@@ -0,0 +1,7 @@
+(ns Stencil.test.transform
+  (:use [stencil.transform])
+  (:use [clojure.test]))
+
+;(deftest identities
+; (is (= (normalize '(a)) '(a)))
+; (is (= (normalize '(stencil program (a))) '(stencil program (a)))))
