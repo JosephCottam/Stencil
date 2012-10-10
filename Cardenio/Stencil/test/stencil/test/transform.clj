0e39edf224fba343cef3efa4be5e6c4fd9b3a890
Joseph Cottam <jcottam@indiana.edu>
2012-10-09 23:58:12 -0400

    WIP: Normalize tests.

--- Cardenio/Stencil/test/stencil/test/transform.clj
+++ Cardenio/Stencil/test/stencil/test/transform.clj
@@ -2,6 +2,16 @@
   (:use [stencil.transform])
   (:use [clojure.test]))
 
-;(deftest identities
-; (is (= (normalize '(a)) '(a)))
-; (is (= (normalize '(stencil program (a))) '(stencil program (a)))))
+(deftest tag
+ (is (= (normalize 'a) '($id a)))
+ (is (= (normalize '(stencil program a)) '(stencil ($id program) ($id a))))
+ (is (= (normalize '(stencil program (table plot (data (range 0 1)))))
+        '(stencil ($id program) (table ($id plot) ($policy data (($op range) 0 1)))))))
+
+(deftest liftInfix
+  (is (= (normalize '(a + b)) '(($op +) ($id a) ($id b))))
+  (is (= (normalize '(+ a b) '(($op +) ($id a) ($id b)))))
+  (is (= (normalize '(map +' ls)) '(($op map) ($id +) ($id ls))))
+  (is (= (normalize '(a plus' b) '(($op plus) ($id a) ($id b)))))
+  (is (= (normalize '(map plus ls)) '(($op map) ($id plus) ($id ls)))))
+
