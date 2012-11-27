28ffbf45a00b2b6e0e1c34dd638721ff61c7b2a7
Joseph Cottam <jcottam@indiana.edu>
2012-11-27 15:36:20 -0500

    Fixed import definition, parse tests now running again.

--- Cardenio/Stencil/test/stencil/test/core.clj
+++ Cardenio/Stencil/test/stencil/test/core.clj
@@ -1,6 +1,6 @@
 (ns Stencil.test.core
-  (:require {stencil.core :as c})
-  (:use {clojure.test}))
+  (:require [stencil.core :as c])
+  (:use [clojure.test]))
 
 (deftest parseStencil
   (is (= (c/parseStencil "(a)") '(a)))
@@ -10,7 +10,9 @@
   (is (= (c/parseStencil "##(a b c)") '($ptuple a b c)))
   (is (= (c/parseStencil "##())") '($ptuple)))
   (is (= (c/parseStencil "(a : {int})") '(a ($meta int))))
+  (is (= (c/parseStencil "[a]") '(tuple-ref a)))
   (is (= (c/parseStencil "(a : b)") '(a $C b)))
   (is (= (c/parseStencil "; some") '(comment " some")))
   (is (= (c/parseStencil ";* some then more*;")) '(comment " some then more"))
-  (is (= (c/parseStencil "(;* some*; then more)")) '((comment " some") then more)))
+  (is (= (c/parseStencil "(;* some*; then more)")) '((comment " some") then more)
+  ))
