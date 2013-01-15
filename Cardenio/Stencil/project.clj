aa988450a499ba7b48dc19d9cbc7a030b39e31d0
Joseph Cottam <jcottam@indiana.edu>
2013-01-15 15:50:53 -0500

    Infrastructure for StringTemplate.

--- Cardenio/Stencil/project.clj
+++ Cardenio/Stencil/project.clj
@@ -1,8 +1,9 @@
 (defproject stencil "0.0.1-SNAPSHOT"
   :description "Stencil -- Cardenio remake!"
   :url "http://cs.indiana.edu/~jcottam/stencil.xml"
+  :repositories {"local" ~(str (.toURI (java.io.File. "locallib")))}  ;;Because I couldn't find a consistently updated Stringtemplate in external repos
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.match "0.2.0-alpha11"]
-                 [org.clojure/tools.trace "0.7.3"]]) ;Development only..
+                 [org.antlr/stringtemplate "4.0.7"]]
+  :dev-dependencies [[org.clojure/tools.trace "0.7.3"]])
 
-;  :profiles {:def {:dependencies [[org.clojure/tools.trace "0.7.3"]]}})
