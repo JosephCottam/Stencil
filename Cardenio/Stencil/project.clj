9c2708ffed7ec0d1e9114eab10abc0afaa9de2c2
Joseph Cottam <jcottam@indiana.edu>
2013-01-15 15:53:31 -0500

    Better placement of dependency.

--- Cardenio/Stencil/project.clj
+++ Cardenio/Stencil/project.clj
@@ -2,7 +2,5 @@
   :description "Stencil -- Cardenio remake!"
   :url "http://cs.indiana.edu/~jcottam/stencil.xml"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.match "0.2.0-alpha11"]
-                 [org.clojure/tools.trace "0.7.3"]]) ;Development only..
-
-;  :profiles {:def {:dependencies [[org.clojure/tools.trace "0.7.3"]]}})
+                 [org.clojure/core.match "0.2.0-alpha11"]]
+  :dev-dependencies [[org.clojure/tools.trace "0.7.3"]])
