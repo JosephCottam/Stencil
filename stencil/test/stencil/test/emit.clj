ba14eed4a49d52d60b8430dd7c690cbccf2d00ca
Joseph Cottam <jcottam@indiana.edu>
2013-01-31 16:09:59 -0500

    Bokeh tests expanded to include multiplot.

--- stencil/test/stencil/test/emit.clj
+++ stencil/test/stencil/test/emit.clj
@@ -1,4 +1,5 @@
 (ns stencil.test.emit
+  (:require [stencil.emitters.bokeh :as bokeh])
   (:require [stencil.emitters.cdx :as cdx])
   (:require [stencil.core :as c])
   (:use [clojure.test]))
@@ -12,3 +13,15 @@
     (is (= (.trim (cdx/emit (c/compile (str base "cdx/scatterplot-twoTable.stencil"))))
            (.trim (slurp (str base "cdx/scatterplot-twoTable.py"))))
         "Scatterplot: Two tables, inline render"))
+
+(deftest bokeh
+    (is (= (.trim (bokeh/emit (c/compile (str base "bokeh/scatterplot-inline.stencil"))))
+           (.trim (slurp (str base "bokeh/scatterplot-inline.py"))))
+        "Scatterplot: One table, inline render")
+    (is (= (.trim (bokeh/emit (c/compile (str base "bokeh/scatterplot-twoTable.stencil"))))
+           (.trim (slurp (str base "bokeh/scatterplot-twoTable.py"))))
+        "Scatterplot: Two tables, inline render")
+    (is (= (.trim (bokeh/emit (c/compile (str base "bokeh/multiplot.stencil"))))
+           (.trim (slurp (str base "bokeh/multiplot.py"))))
+        "Scatterplot: Multiplot"))
+
