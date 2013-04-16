f09b71b9eb462d8ecc037612a07c25dab5bcc75c
Joseph Cottam <jcottam@indiana.edu>
2013-04-15 20:48:44 -0400

    Modified vega test equality condition.

--- stencil/test/stencil/test/vega.clj
+++ stencil/test/stencil/test/vega.clj
@@ -2,6 +2,7 @@
   (:require [stencil.emitters.vega :as vega])
   (:require [stencil.core :as c])
   (:require [stencil.transform :as t])
+  (:require [clojure.data.json :as json])
   (:use [clojure.test])
   (:use [stencil.test.utils]))
 
@@ -15,8 +16,8 @@
          src# (str source "/" base# ".stencil")
          ref# (str source "/" base# ".js")
          rslt# (str target "/" base# ".js")
-         result# (.trim (emit emitter# target src# rslt#))
-         expected# (.trim (slurp ref#))]
+         result# (json/read-str (.trim (emit emitter# target src# rslt#)))
+         expected# (json/read-str (.trim (slurp ref#)))]
      (if (= result# expected#)
        (report {:type :pass :message ~msg, :expected ref# :actual rslt#})
        (report {:type :fail :message (str "Compile did not match --- " ~msg), :expected ref# :actual rslt#}))
